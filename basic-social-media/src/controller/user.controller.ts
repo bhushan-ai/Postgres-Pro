f393fdeb12889de98da9d632fdae909bf66c7905
bhushan-ai <bhushaningole2004@gmail.com>
2026-04-28 12:52:17 +0530

    user crud operations completed

--- basic-social-media/src/controller/user.controller.ts
+++ basic-social-media/src/controller/user.controller.ts
@@ -1,6 +1,7 @@
 import { prisma } from "../lib/prisma";
 import { Request, Response } from "express";
 
+// create user
 export const createUser = async (
   req: Request,
   res: Response,
@@ -28,6 +29,105 @@ export const createUser = async (
   } catch (error: unknown) {
     const err = error as Error;
     console.log(`something went wrong while registering the user`, err);
+    res
+      .status(500)
+      .json({ success: false, message: "Server side error", error: err });
+  }
+};
+
+// update user
+export const updateUser = async (
+  req: Request,
+  res: Response,
+): Promise<void> => {
+  try {
+    const userId = req.params.id;
+    const { name, email, password } = req.body;
+
+    const updatedUser = await prisma.user.update({
+      where: {
+        id: Number(userId),
+      },
+      data: {
+        name: name,
+        email: email,
+        password: password,
+      },
+    });
+
+    res
+      .status(201)
+      .json({ success: true, message: "User updated ", data: updatedUser });
+  } catch (error: unknown) {
+    const err = error as Error;
+    console.log(`something went wrong while updating the user`, err);
+    res
+      .status(500)
+      .json({ success: false, message: "Server side error", error: err });
+  }
+};
+
+// fetch users
+export const fetchUsers = async (
+  req: Request,
+  res: Response,
+): Promise<void> => {
+  try {
+    const allUsers = await prisma.user.findMany();
+    res
+      .status(200)
+      .json({ success: true, message: "Users fetched ", data: allUsers });
+  } catch (error: unknown) {
+    const err = error as Error;
+    console.log(`something went wrong while getting the user`, err);
+    res
+      .status(500)
+      .json({ success: false, message: "Server side error", error: err });
+  }
+};
+
+// get one user
+export const showUser = async (req: Request, res: Response): Promise<void> => {
+  try {
+    const userId = req.params.id;
+
+    const user = await prisma.user.findFirst({
+      where: {
+        id: Number(userId),
+      },
+    });
+    res
+      .status(200)
+      .json({ success: true, message: "Users fetched ", data: user });
+  } catch (error: unknown) {
+    const err = error as Error;
+    console.log(`something went wrong while getting the user`, err);
+    res
+      .status(500)
+      .json({ success: false, message: "Server side error", error: err });
+  }
+};
+
+// delete user
+export const deleteUser = async (
+  req: Request,
+  res: Response,
+): Promise<void> => {
+  try {
+    const userId = req.params.id;
+
+    const deletedUser = await prisma.user.delete({
+      where: {
+        id: Number(userId),
+      },
+    });
+
+    res
+      .status(200)
+      .json({ success: true, message: "User deleted ", data: deletedUser });
+  } catch (error: unknown) {
+    const err = error as Error;
+    console.log(`something went wrong while deleting the user`, err);
     res
       .status(500)
       .json({ success: false, message: "Server side error", error: err });
--- basic-social-media/src/routes/user.routes.ts
+++ basic-social-media/src/routes/user.routes.ts
@@ -1,8 +1,18 @@
 import express from "express";
-import { createUser } from "../controller/user.controller";
+import {
+  createUser,
+  deleteUser,
+  fetchUsers,
+  showUser,
+  updateUser,
+} from "../controller/user.controller";
 
 const userRoutes = express.Router();
 
 userRoutes.post("/create-user", createUser);
+userRoutes.get("/users", fetchUsers);
+userRoutes.get("/user/:id", showUser);
+userRoutes.put("/update-user/:id", updateUser);
+userRoutes.delete("/delete-user/:id", deleteUser);
 
 export default userRoutes;
