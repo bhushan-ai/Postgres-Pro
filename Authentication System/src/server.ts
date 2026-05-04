81fe5ad27df9478f3f7aa189f004e4200a0b1016
bhushan-ai <bhushaningole2004@gmail.com>
2026-05-04 13:34:50 +0530

    project initialized

---
+++ Authentication System/src/server.ts
@@ -0,0 +1,11 @@
+import "dotenv/config";
+import express, { Request, Response } from "express";
+
+const app = express();
+
+// Routes
+app.get("/", (req: Request, res: Response) => {
+  return res.send("API's are working!!");
+});
+
+export default app;
