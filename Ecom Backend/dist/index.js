1dc861b376882044fc3471b7b26ea329cabc7168
bhushan-ai <bhushaningole2004@gmail.com>
2026-05-19 14:50:04 +0530

    project successfully deployed on Docker render supabase Postgress Prisma

--- Ecom Backend/dist/index.js
+++ Ecom Backend/dist/index.js
@@ -1,5 +1,5 @@
 import app from "./server.js";
 const PORT = process.env.PORT || 4000;
 app.listen(PORT, () => {
-  console.log(`server started on http://localhost:${PORT}`);
+    console.log(`server started on http://localhost:${PORT}`);
 });
