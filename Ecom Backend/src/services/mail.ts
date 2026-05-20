250528763395cabce32edc32535144abaf490928
bhushan-ai <bhushaningole2004@gmail.com>
2026-05-20 14:17:49 +0530

    project successfully deployed using Docker render supabase Postgress Prisma and smpt fixed

--- Ecom Backend/src/services/mail.ts
+++ Ecom Backend/src/services/mail.ts
@@ -6,7 +6,11 @@ export const transporter = nodemailer.createTransport({
   secure: false,
 
   auth: {
-    user: process.env.EMAIL_USER,
-    pass: process.env.EMAIL_PASS,
+    user: process.env.EMAIL_USER!,
+    pass: process.env.EMAIL_PASS!,
+  },
+  
+  tls: {
+    rejectUnauthorized: false,
   },
 });
